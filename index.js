f66f08709e39a9f37a908a8dbb230b03dfaeaef5
Craig Christensen <ruffcorn@gmail.com>
2018-03-14 15:07:26 -0500

    initial commit

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ server.listen(8080);
 app.use(express.static(__dirname + '/public'));
 console.log("Express server running on port 8080");
 
+// array of all lines drawn
+var line_history = [];
+
 // event-handler for new incoming connections
 io.on('connection', function (socket) {
-  var line_history = [];
   // first send the history to the new client
   for (var i in line_history) {
      socket.emit('draw_line', { line: line_history[i] } );
